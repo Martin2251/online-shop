074098e7c4c9a108e56861eeb55b355e99d1b5b2
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-06-16 07:56:02 +0100

    added

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require ("express");
+const cors = require("cors");
+const products = require("./products");
+const mongoose = require("mongoose")
+
+
+
+const app = express();
+
+require("dotenv").config()
+
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to our online shop API")
+});
+
+app.get("/products", (req, res) => {
+    res.send(products);
+})
+
+const port = process.env.PORT || 8000
+const uri = process.env.DB_URI
+
+app.listen(port, console.log(`Server running on port ${port}`))
+
+mongoose.connect(uri,{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+}).then (() => console.log("MongoDb successful"))
+.catch((err ) => console.log("Mongo DB connection failed", err.message));
---
+++ backend/products.js
@@ -0,0 +1,24 @@
+const products = [
+    {
+        id: 1,
+        name: "Iphone 12 Pro",
+        desc: "6.1 inch display",
+        price: 999,
+        image: "https://res.cloudinary.com/dbshzf2px/image/upload/v1654344733/onlineShop/iphone12pro_htsd8n.jpg"
+    },
+    {
+        id: 2,
+        name: "Iphone 12",
+        desc: "5.4 inch display",
+        price: 699,
+        image: "https://res.cloudinary.com/dbshzf2px/image/upload/v1654344733/onlineShop/iphone12_ypdmxi.jpg"
+    },
+    {
+        id: 3,
+        name: "Galaxy S",
+        desc: "6.5 inch display",
+        price: 399,
+        image: "https://res.cloudinary.com/dbshzf2px/image/upload/v1654344733/onlineShop/galaxyS_ao2v4k.png"
+    },
+];
+module.exports = products;
